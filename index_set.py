6bfff4ebe170aa6a44c209432f947019fb953667
VasanthOPT <56574647+VasanthOPT@users.noreply.github.com>
2019-10-15 10:28:24 +0530

    Create index_set.py

---
+++ index_set.py
@@ -0,0 +1,12 @@
+def index_collection(x): # let x be any non empty list then 
+  index_collect=[]
+  for i in x:
+    a_i=x.index(i)
+    index_collect.append(a_i)
+  print(index_collect)
+Alphabets=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+index_collection(A)
+
+#Standard ouput:
+#[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
+    
